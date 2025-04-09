afdaff96d3abaf72834a74065b643d70a9c35500
Adamgzlez <118874236+Adamgzlez@users.noreply.github.com>
2025-04-09 17:28:54 -0500

    WN10-AC-000010.ps1

---
+++ STIGS/WN10-AC-000010.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Adam Gonzalez
+    LinkedIn        : linkedin.com/in/joshmadakor/
+    GitHub          : github.com/joshmadakor1
+    Date Created    : 2025-04-09
+    Last Modified   : 2025-04-09
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000010
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN10-AC-000010-Remediation.ps1 
+#>
+
+# Set Account Lockout Threshold: 3 invalid logon attempts
+net accounts /lockoutthreshold:3
+
+# Output a summary of the configured account lockout policy
+Write-Host "- Lockout threshold: 3 attempts"
