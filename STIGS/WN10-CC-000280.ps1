ff9191bd0bfeb9519565850560d190499fc88b5a
Adamgzlez <118874236+Adamgzlez@users.noreply.github.com>
2025-04-10 23:08:36 -0500

    Update WN10-CC-000280.ps1

--- STIGS/WN10-CC-000280.ps1
+++ STIGS/WN10-CC-000280.ps1
@@ -1,1 +1,40 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Josh Madakor
+    LinkedIn        : linkedin.com/in/joshmadakor/
+    GitHub          : github.com/joshmadakor1
+    Date Created    : 2024-09-09
+    Last Modified   : 2024-09-09
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000280
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__WN10-CC-000280-Remediation.ps1 
+#>
+
+# Define the registry path
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+
+# Check if the key exists, if not, create it
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force
+}
+
+# Set the DWORD value
+New-ItemProperty -Path $registryPath -Name "fPromptForPassword" -PropertyType DWord -Value 1 -Force
+
+Write-Output "Registry key set successfully."
 
