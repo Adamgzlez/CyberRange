27aaeb76ba1adbee892f4809d755a74e65c73a97
Adamgzlez <118874236+Adamgzlez@users.noreply.github.com>
2025-04-10 21:11:06 -0500

    Create WN10-CC-000295.ps1

---
+++ STIGS/WN10-CC-000295.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Josh Madakor
+    LinkedIn        : linkedin.com/in/joshmadakor/
+    GitHub          : github.com/joshmadakor1
+    Date Created    : 2024-09-09
+    Last Modified   : 2024-09-09
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000295 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN10-CC-000295-Remediation.ps1 
+#>
+
+# PowerShell Script to set DisableEnclosureDownload in the registry
+
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Feeds"
+$valueName = "DisableEnclosureDownload"
+$valueData = 1
+
+# Check if the registry path exists, if not, create it
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force
+}
+
+# Set the registry value
+New-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -PropertyType DWord -Force
+
+Write-Output "Registry key has been set successfully."
