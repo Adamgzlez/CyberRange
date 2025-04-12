f46f54deb6141a686cc2934f9d5e465310b222bb
Adamgzlez <118874236+Adamgzlez@users.noreply.github.com>
2025-04-12 03:13:40 -0500

    Update WN10-CC-000280.ps1

--- STIGS/WN10-CC-000280.ps1
+++ STIGS/WN10-CC-000280.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures that Remote Desktop Services always prompt a client for passwords upon connection.
 
 .NOTES
     Author          : Adam Gonzalez
