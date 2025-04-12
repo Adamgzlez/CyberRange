ab7dc70ddb4ce0e681388ba9295b50a285f89e14
Adamgzlez <118874236+Adamgzlez@users.noreply.github.com>
2025-04-12 03:01:16 -0500

    Update WN10-AC-000015.ps1

--- STIGS/WN10-AC-000015.ps1
+++ STIGS/WN10-AC-000015.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures that the period of time before the bad logon counter is reset is configured to 15 minutes.
 
 .NOTES
     Author          : Adam Gonzalez
