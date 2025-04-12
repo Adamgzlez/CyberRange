5127f26fdb4f68a590792f0b09fb7e3d3a819115
Adamgzlez <118874236+Adamgzlez@users.noreply.github.com>
2025-04-12 03:20:37 -0500

    Update WN10-CC-000390.ps1

--- STIGS/WN10-CC-000390.ps1
+++ STIGS/WN10-CC-000390.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures that Windows 10 is configured to prevent users from receiving suggestions for third-party or additional applications.
 
 .NOTES
     Author          : Adam Gonzalez
