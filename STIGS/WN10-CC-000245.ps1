7ecdf53a9a1b964ac0ff0f154d2644f938e224f9
Adamgzlez <118874236+Adamgzlez@users.noreply.github.com>
2025-04-12 02:36:00 -0500

    Update WN10-CC-000245.ps1

--- STIGS/WN10-CC-000245.ps1
+++ STIGS/WN10-CC-000245.ps1
@@ -7,7 +7,7 @@
     LinkedIn        : linkedin.com/in/adamgzlez
     GitHub          : github.com/Adamgzlez
     Date Created    : 2024-04-09
-    Last Modified   : 2024-04-11
+    Last Modified   : 2024-04-12
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
