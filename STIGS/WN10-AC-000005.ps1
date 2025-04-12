336ddda0422304dab67908ec78fdd9bbfd411e30
Adamgzlez <118874236+Adamgzlez@users.noreply.github.com>
2025-04-12 02:57:43 -0500

    Update WN10-AC-000005.ps1

--- STIGS/WN10-AC-000005.ps1
+++ STIGS/WN10-AC-000005.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures that the Windows 10 account lockout duration is configured to 15 minutes or greater.
 
 .NOTES
     Author          : Adam Gonzalez
